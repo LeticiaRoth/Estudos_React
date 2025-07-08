77d262c7c8f33015723ceb3eed49d21cb13d6141
LeticiaRoth <leticiaroth1@gmail.com>
2025-07-08 11:21:05 -0300

    Testes do react

--- my-app/src/Teste2.js
+++ my-app/src/Teste2.js
@@ -1,11 +1,26 @@
-import React from 'react';
+
+import React, { useState,useEffect} from 'react';
 
 import './Teste2.css';
 
 function Teste2() {
+  const [count, setContagem] = useState(0);
+
+  //Atualiza o nome da página conforme o usuário clica no botão
+  useEffect(() => {
+    document.title = `Você clicou ${count} vezes`;
+    
+  });
+
   return (
     <div className='Teste2'>
         <h2>TESTE DE EFEITO</h2>
+
+        <div className="containerTeste">
+        <p>Você clicou {count} vezes</p>
+        <button onClick={() => setContagem(count + 1)}>Clique aqui!</button>
+        <button onClick={() => setContagem(0)}>Resetar</button>
+      </div>
     </div>
   );
 }
